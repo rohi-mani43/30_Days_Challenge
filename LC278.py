a2a1c313ac435ec9833f1936fe774ee7833391b1
Rohith Manikanta Peruri <165360028+rohi-mani43@users.noreply.github.com>
2025-08-31 20:11:04 +0530

    LC278.py

---
+++ LC278.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid   # first bad is mid or before
+            else:
+                left = mid + 1  # first bad is after mid
+        return left
