ccf91c43ce0fdd4aed41320504fd92acc1ac32e8
Rohith Manikanta Peruri <165360028+rohi-mani43@users.noreply.github.com>
2025-08-31 20:03:06 +0530

    Day 12 Challenge

---
+++ PeakElement.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        
+        return arr.index(max(arr))
---
+++ PerfectSquare.py
@@ -0,0 +1,4 @@
+import math
+a=int(input())
+b=math.sqrt(a)
+if b%10==0:print(b)
