b649325a997d8422fb5989214aa8d50f1a07bcde
Rohith Manikanta Peruri <165360028+rohi-mani43@users.noreply.github.com>
2025-09-11 22:28:45 +0530

    Day23 Challenge

---
+++ BTLevelOrder.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        res = []
+        queue = [root]
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+
+        return res
