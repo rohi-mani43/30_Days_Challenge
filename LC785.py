35c9f6075b40693d26fc40d874e7078d4a6fb103
Rohith Manikanta Peruri <165360028+rohi-mani43@users.noreply.github.com>
2025-09-16 10:38:27 +0530

    Day27 Challenge

---
+++ LC785.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isBipartite(self, graph):
+        n = len(graph)
+        color = [-1] * n
+        
+        def dfs(node, c):
+            color[node] = c
+            for nei in graph[node]:
+                if color[nei] == -1:
+                    if not dfs(nei, 1 - c):
+                        return False
+                elif color[nei] == c:
+                    return False
+            return True
+        
+        for i in range(n):
+            if color[i] == -1 and not dfs(i, 0):
+                return False
+        return True
